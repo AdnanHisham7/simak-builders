88857ff741047df625ea5e395fc113185fab9fa1
AdnanHisham7 <a.hishamkk@gmail.com>
2025-08-16 01:22:26 +0530

    Change zoho provider back to gmail

--- server/services/emailService.ts
+++ server/services/emailService.ts
@@ -2,11 +2,9 @@ import nodemailer from 'nodemailer';
 import { env } from '@config/env';
 
 const transporter = nodemailer.createTransport({
-  host: 'smtp.zoho.com',
-  port: 587,
-  secure: false,
+  service: 'gmail',
   auth: {
-    user: env.EMAIL_USER, 
+    user: env.EMAIL_USER,
     pass: env.EMAIL_PASS,
   },
 });
